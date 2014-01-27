8a15833feffbf11a6bf1c02c4f8d3b48554593f9
huyd <huy.dinh@eastagile.com>
2014-01-27 15:46:05 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HaiMut::Application.config.secret_key_base = 'bdb5ef43948cbf256202e67aeece3373560679bcaa5698f85d2d113425cb88571c17698a4ef4d946ef766e5f08888f4bd72f3477da68ead21429dc8bee1bdfbe'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HaiMut::Application.config.session_store :cookie_store, key: '_HaiMut_session'
